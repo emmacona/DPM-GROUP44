1a499c2144ae6745095694d02bc522b817fea5af
garrettk <garrett@humanitas.io>
2019-09-26 14:22:54 -0400

    Added starting code for lab 3

---
+++ src/ca/mcgill/ecse211/lab3/Navigation.java
@@ -0,0 +1,35 @@
+package ca.mcgill.ecse211.lab3;
+
+import static ca.mcgill.ecse211.lab3.Resources.*;
+
+public class Navigation {
+
+   /**
+    * 
+    * @param x
+    * @param y
+    */
+   public static void travelTo(double x, double y) {
+     
+   }
+   
+   /**
+    * 
+    * @param theta
+    */
+   public static void turnTo(double theta) {
+     
+   }
+   
+   /**
+    * 
+    * @return
+    */
+   public static boolean isNavigating() {
+     boolean isNavigating = false;
+     
+     // TODO: check if travelTo or turnTo has been called without returning
+     
+     return isNavigating;
+   }
+}
