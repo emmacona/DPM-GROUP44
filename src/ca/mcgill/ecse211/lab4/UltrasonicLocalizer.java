a41bbd8cf03d9475a3f648ef38d16a75c8b9a7a3
emmacona <emmanuelle.coutu-nadeau@mail.mcgill.ca>
2019-10-03 23:38:47 -0400

    skeletton for lab4

---
+++ src/ca/mcgill/ecse211/lab4/UltrasonicLocalizer.java
@@ -0,0 +1,26 @@
+package ca.mcgill.ecse211.lab4;
+
+public class UltrasonicLocalizer {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+	/**
+	 *  Falling edge method.
+	 */
+	private static void fallingEdge() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/**
+	 *  Rising edge method.
+	 */
+	private static void risingEdge() {
+		// TODO Auto-generated method stub
+		
+	}
+}
